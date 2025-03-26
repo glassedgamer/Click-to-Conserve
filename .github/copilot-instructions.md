5a56ad3238771b1564464c0a351101fe665fd1c5
glassedgamer <glassedgamer05@gmail.com>
2025-03-26 09:19:16 -0400

    Upgrades pt. 1
    Scriptable Objects

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -2,7 +2,7 @@
 - Project name: Click to Conserve
 - Unity version: Unity 2022.3.34f1
 - Active game object:
-  - Name: MONEY text
+  - Name: Upgrades Manager
   - Tag: Untagged
-  - Layer: UI
+  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Scripts/Upgrades.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Upgrades : ScriptableObject
+{
+    public new string name;
+
+    public int moneyPerSecond;
+    public int moneyPerClick;
+}
