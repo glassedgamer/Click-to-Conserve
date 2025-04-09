3e5332f4a413f57524eb3d4333dc7f247ce46dbc
glassedgamer <36608767+glassedgamer@users.noreply.github.com>
2025-04-09 15:12:01 -0400

    Switching over to WebGL

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,6 +13,14 @@ public class GameManager : MonoBehaviour
 
     public float moneyMultiplier = 1f;
     public float moneyPerSecond = 0.1f;
+    public float loseMoneyVal;
+    
+    [Header("Game Completion Stuff")]
+    public float moneyWinCondition;
+    public float moneyLoseCondition;
+
+    [SerializeField] GameObject winScreen;
+    [SerializeField] GameObject loseScreen;
 
     [Header("Time Stuff")]
     [SerializeField] float startingTime;
@@ -29,6 +38,9 @@ public class GameManager : MonoBehaviour
         wasteSlider.maxValue = maxWaste;
 
         moneyText.text = "Money: $" + money.ToString();
+
+        winScreen.SetActive(false);
+        loseScreen.SetActive(false);
     }
 
     void Update() {
@@ -36,10 +48,21 @@ public class GameManager : MonoBehaviour
 
         moneyText.text = "Money: $" + money.ToString("0");
 
+        // Relation between current value and max value
         if(currentWaste < maxWaste)
         {
             Timer();
         }
+        if(currentWaste == maxWaste) {
+            // Reseting waste once it reaches max val
+            currentWaste = 0;
+            wasteSlider.value = currentWaste;
+
+            money -= loseMoneyVal;
+            moneyText.text = "Money: $" + money.ToString();
+        }
+
+        WinCondition();
         LoseCondition();
     }
 
@@ -50,9 +73,10 @@ public class GameManager : MonoBehaviour
         moneyText.text = "Money: $" + money.ToString("");
     }
 
+    // Counts down and when reaches 0, throw out food and reset timer
     void Timer()
     {
-        if(remainingTime > 0)
+        if(remainingTime > 0 && money < moneyWinCondition && money > moneyLoseCondition)
         {
             remainingTime -= Time.deltaTime;
         } else if (remainingTime < 0)
@@ -72,20 +96,28 @@ public class GameManager : MonoBehaviour
         //Do the thing
         currentWaste++;
         wasteSlider.value = currentWaste;
-
-        print(":)");
     }
 
     void WinCondition()
     {
         // YOU WIN WOOOOO
+        if(money >= moneyWinCondition) {
+            winScreen.SetActive(true);
+            RestartGame();
+        }
     }
     
     void LoseCondition()
     {
-        if(currentWaste == maxWaste)
-        {
-            print("LOSER");
+        if(money <= moneyLoseCondition) {
+            loseScreen.SetActive(true);
+            RestartGame();
+        }
+    }
+
+    void RestartGame() {
+        if(Input.GetKeyDown(KeyCode.R)) {
+            SceneManager.LoadScene("MainGame");
         }
     }
 }
--- Assets/Scripts/Upgrades/UpgradeManager.cs
+++ Assets/Scripts/Upgrades/UpgradeManager.cs
@@ -12,19 +12,18 @@ public class UpgradeManager : MonoBehaviour
 
     public List<Upgrades> availableUpgrades;
     public List<Button> upgradeButtons;
-    public int clickPower = 1;
 
     [SerializeField] GameManager gameManager;
 
     void Start() {
-        // Sets the info of each upgrade button to the data from each scriptable object
-        for(int i = 0; i < upgradeButtons.Count; i++) {
-            upgradeButtons[i].GetComponent<UpgradeButton>().upgradeInfo = availableUpgrades[i];
-            upgradeButtons[i].GetComponent<UpgradeButton>().InitializeInfo();
-        }
+        SetPurchaseButtons();
     }
 
     void Update() {
+        SetPurchaseButtons();
+    }
+
+    void SetPurchaseButtons() {
         // Sets the info of each upgrade button to the data from each scriptable object
         for(int i = 0; i < upgradeButtons.Count; i++) {
             upgradeButtons[i].GetComponent<UpgradeButton>().upgradeInfo = availableUpgrades[i];
